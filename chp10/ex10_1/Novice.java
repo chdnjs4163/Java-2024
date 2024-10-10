6596fff255e646dc0220847fe0ac7daa628ad074
chdnjs4163 <162400165+chdnjs4163@users.noreply.github.com>
2024-10-10 13:34:47 +0900

    Add files via upload

---
+++ chp10/ex10_1/Novice.java
@@ -0,0 +1,13 @@
+package chp10.ex10_1;
+
+public class Novice {
+
+    String name;
+    int hp;
+    void punch(){
+        System.out.printf("%s(HP: %d)의 편치\n", name, hp);
+    }
+
+
+
+}
---
+++ chp10/ex10_1/wizard.java
@@ -0,0 +1,9 @@
+package chp10.ex10_1;
+
+public class wizard extends Novice {
+
+    int mp;
+    void fireball(){
+        System.out.printf("%s(HP: %d, MP: %d)의 파이어볼 ~00000000000000~~~~~~\n",name,hp,mp);
+    }
+}

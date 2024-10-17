91499815eedb34b333c416045e7c34f719c3b668
chdnjs4163 <162400165+chdnjs4163@users.noreply.github.com>
2024-10-17 10:01:19 +0900

    Add files via upload

---
+++ ex10_4/MianSquare.java
@@ -0,0 +1,11 @@
+package chp10.ex10_4;
+
+public class MianSquare {
+    public static void main(String[] args) {
+        Square square = new Square();
+//      Object, Shape, Square 객체의 필드와 메소드 접근가능
+        Square shape = new Square();
+//      Shape 객체의 필드와 메소드 접근 가능(예외적으로 Squre 객체의 맴버를 접근할 수도 있다.)
+
+    }
+}
